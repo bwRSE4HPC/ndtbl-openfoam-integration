6675faafbbb6e729ea4d87987d9e0f5d3da4c679
Thomas Isensee <26852629+thomasisensee@users.noreply.github.com>
2026-09-20 15:28:07 +0200

    docs: add copyright info to SPDX license identifiers

--- scripts/ndtbl_table_staging.sh
+++ scripts/ndtbl_table_staging.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# SPDX-FileCopyrightText: 2026 Thomas Isensee
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 ndtbl_staging_usage()
 {
     cat <<'EOF'
--- src/combustionModels/tableSolver/tableSolver/tableSolver.C
+++ src/combustionModels/tableSolver/tableSolver/tableSolver.C
@@ -1,3 +1,4 @@
+// SPDX-FileCopyrightText: 2026 Thomas Isensee
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "tableSolver.H"
